d896a7741f39bff24fe9465c48a53b4c73649264
vk18chiku <uttammahato379@gmail.com>
2025-12-30 20:39:24 +0530

    Update DB connection to use environment variables
    Refactored database connection to use environment variables.

--- db_functions.py
+++ db_functions.py
@@ -1,12 +1,14 @@
+
 import pymysql
+import os
 
 def connect_to_db():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        database="dummy_project",
-        password="Uttam1234@",
-        port=3307
+        host=os.environ["DB_HOST"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        port=int(os.environ["DB_PORT"])
     )
 
 
